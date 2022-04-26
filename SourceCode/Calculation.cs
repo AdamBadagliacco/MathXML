67eb41030cd0271441c5868d8881b27a9111a9b1
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2022-04-26 10:25:58 -0500

    Added Files to Repo

---
+++ SourceCode/Calculation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Mathml
+{
+    class Calculation
+    {
+        string username;
+        enum OperationName { SUM, SUB, MULTIPLY, DIVIDE, POWER};
+        OperationName operationName;
+        char operationChar;
+        string miscellaneousInfo;
+
+        float value1;
+        float value2;
+
+        float calculationResult;
+
+        public Calculation(string description, string value1String, string value2String) {
+
+            
+            if (!float.TryParse(value1String, out value1))
+            {
+                value1 = WordsToNumbers.ConvertToFloat(value1String);
+            }
+           
+            if (!float.TryParse(value2String, out value2))
+            {
+                value2 = WordsToNumbers.ConvertToFloat(value2String);
+            }
+
+            int positionOfFirstSemiColon = description.IndexOf(";");
+            int positionOfSecondSemiColon = positionOfFirstSemiColon + description.Substring(positionOfFirstSemiColon+1).IndexOf(";") + 1;
+
+            username = description.Substring(0, positionOfFirstSemiColon);
+            miscellaneousInfo = description.Substring(positionOfSecondSemiColon+1);
+
+            string operation = description.Substring(positionOfFirstSemiColon+1, positionOfSecondSemiColon- positionOfFirstSemiColon-1);
+
+            if (operation.Contains("SUM"))
+            {
+                operationName = OperationName.SUM;
+                operationChar = '+';
+                calculationResult = value1 + value2;
+            }
+            else if (operation.Contains("SUB"))
+            {
+                operationName = OperationName.SUB;
+                operationChar = '-';
+                calculationResult = value1 - value2;
+            }
+            else if (operation.Contains("MULTIPLY"))
+            {
+                operationName = OperationName.MULTIPLY;
+                operationChar = '*';
+                calculationResult = value1 * value2;
+            }
+            else if (operation.Contains("DIVIDE"))
+            {
+                operationName = OperationName.DIVIDE;
+                operationChar = '/';
+                calculationResult = value1 / value2;
+            }
+            else if (operation.Contains("POWER"))
+            {
+                operationName = OperationName.POWER;
+                operationChar = '^';
+                calculationResult = (float)Math.Pow((double)value1, (double)value2);
+            }
+        }
+
+        public string GetCalculation() {
+            return username + " – " + operationName.ToString() + " – " + value1 + " " + operationChar + " " + value2 + " = " + calculationResult;
+        }
+
+        public void PrintToDebugObject()
+        {
+            Console.WriteLine("username: " + username);
+            Console.WriteLine("operationName: " + operationName);
+            Console.WriteLine("operationChar: " + operationChar);
+            Console.WriteLine("miscellaneousInfo: " + miscellaneousInfo);
+
+            Console.WriteLine("value1: " + value1);
+            Console.WriteLine("value2: " + value2);
+
+            Console.WriteLine("calculationResult: " + calculationResult);
+        }
+
+
+    }
+}
---
+++ SourceCode/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Xml;
+
+//Press F5 to Run
+
+namespace Mathml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlDocument xmlDoc = new XmlDocument(); 
+            xmlDoc.Load("math.xml");
+
+            XmlNodeList descriptions = xmlDoc.GetElementsByTagName("Description");
+            XmlNodeList firstValues = xmlDoc.GetElementsByTagName("Value1");
+            XmlNodeList secondValues = xmlDoc.GetElementsByTagName("Value2");
+
+            //Verify equal amount of Elements in each list so calculations can be successfully performed
+            if (descriptions.Count != firstValues.Count || descriptions.Count != secondValues.Count) {
+                throw new ArgumentException("XML must contain the same amount of Description, Value1, and Value2 elements");
+            }
+
+            for (int i = 0; i < descriptions.Count; i++){
+                Calculation myCalculation = new Calculation(descriptions[i].InnerText, firstValues[i].InnerText, secondValues[i].InnerText);
+                Console.WriteLine(myCalculation.GetCalculation());
+
+            }
+
+            Console.WriteLine("Press Enter to Exit program");
+            ConsoleKeyInfo lastKeyPressed = Console.ReadKey(true); //true here mean we won't output the key to the console, just cleaner in my opinion.
+            while (lastKeyPressed.Key != ConsoleKey.Enter)
+            {
+                lastKeyPressed = Console.ReadKey(true);
+            }
+        }
+
+        #region Static Methods to Debug
+        static void TestCalculationClass(string description, string value1, string value2)
+        {
+
+            Calculation myCalculation = new Calculation(description, value1, value2);
+
+            Console.WriteLine("Joe – SUM – 40 + 2 = 42");
+            Console.WriteLine(myCalculation.GetCalculation());
+            
+        }
+
+        static void TestCalculationClass()
+        {
+            Console.WriteLine("---Testing functionality of the Calculation Class---");
+
+            string correctResult = "Joe – SUM – 40 + 2 = 42";
+
+            string description = "Joe;SUM;TURN10";
+            string value1 = "40";
+            string value2 = "2";
+            Calculation myCalculation = new Calculation(description, value1, value2);
+            string actualResult = myCalculation.GetCalculation();
+
+            if (string.Equals(correctResult, actualResult)) {
+                Console.WriteLine("Success! The actual result was the correct result.");
+                Console.WriteLine("correctResult: " + correctResult);
+                Console.WriteLine("actualResult: " + actualResult);
+            }
+            else {
+                Console.WriteLine("Failure! The actual result was NOT the correct result.");
+                Console.WriteLine("correctResult: " + correctResult);
+                Console.WriteLine("actualResult: " + actualResult);
+            }
+
+            myCalculation.PrintToDebugObject();
+        }
+        #endregion
+    }
+}
